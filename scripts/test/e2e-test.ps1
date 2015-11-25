2eac9bd8e625e33a1cd8cb94ccb8dadfe315b525
Bryan <brthor@microsoft.com>
2015-11-24 18:12:32 -0800

    Add Native Compilation workaround with vcvarsall.bat invocation.

--- scripts/test/e2e-test.ps1
+++ scripts/test/e2e-test.ps1
@@ -7,12 +7,37 @@
 
 # Restore and compile the test app
 dotnet restore "$RepoRoot\test\E2E" --runtime "osx.10.10-x64" --runtime "ubuntu.14.04-x64" --runtime "win7-x64"
+if (!$?) {
+    Write-Host "Command failed: dotnet restore"
+    Exit 1
+}
+
 dotnet publish --framework dnxcore50 --runtime "$Rid" --output "$RepoRoot\artifacts\$Rid\e2etest" "$RepoRoot\test\E2E"
+if (!$?) {
+    Write-Host "Command failed: dotnet publish"
+    Exit 1
+}
+
+## Temporary Workaround for Native Compilation
+## Need x64 Native Tools Dev Prompt Env Vars
+## Tracked Here: https://github.com/dotnet/cli/issues/301
+pushd "$env:VS140COMNTOOLS\..\..\VC"
+cmd /c “vcvarsall.bat x64&set” |
+foreach {
+  if ($_ -match “=”) {
+    $v = $_.split(“=”); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+  }
+}
+popd
 
 # Run the app and check the exit code
 pushd "$RepoRoot\artifacts\$Rid\e2etest"
 & "CoreRun.exe" "xunit.console.netcore.exe" "E2E.dll"
-if ($LASTEXITCODE -ne 0) {
-    throw "E2E Test Failure"
+if (!$?) {
+    Write-Host "E2E Test Failure"
+    popd
+    Exit 1
 }
-popd
+else {
+    popd
+}
