de116531d58bf8e4e8beaa0f7d884b984c777d72
Krzysztof Wicher <kwicher@microsoft.com>
2016-03-24 17:24:10 -0700

    stub of install scripts test

---
+++ test/Installer/InstallScripts/InstallScripts.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.DotNet.Tools.Test.Utilities;
+using Microsoft.Extensions.PlatformAbstractions;
+using Xunit;
+using System.Diagnostics;
+
+namespace Microsoft.DotNet.Tests.EndToEnd
+{
+    public class InstallScriptsTests : TestBase
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Dummy Entrypoint.");
+        }
+
+        [Fact]
+        public void TestDotnetBuild()
+        {
+            //
+        }
+    }
+}
